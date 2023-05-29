a42657caf43522a53af77f4b5c09d21acea1ec46
Leonid ocampo <115747563+leonidocampo-co@users.noreply.github.com>
2023-05-29 23:05:58 +0000

    cambios en todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 
  
 
 //create your first component
 const Home = () => {
+	const [task, setTask]=useState("")
+	const [todos, setTodos]=useState(["Tarea 1", "Tarea 2"])
+
+	function addTask(e){
+		if(e.code==="Enter"){
+			setTodos([...todos, task])
+			setTask("")
+		}
+
+	}	
+
+	function delTask(index){
+		setTodos([
+			...todos.slice(0, index),
+			...todos.slice(index+1)
+		])
+	}
+
 	return (
-	  <div className="text-center">
+	 
 		<div className="card">
             <div className="card-header">
-			<input type="email" className="form-control border-0" id="exampleFormControlInput1" placeholder="escriba nueva tarea"/>
+			<input
+			 type="text"
+			 className="form-control border-0"  
+			 placeholder="escriba nueva tarea"
+			 value={task}
+			 onChange={(e)=>setTask(e.target.value)}
+			 onKeyDown={addTask}
+			 />
             </div>
+
 			<ul className="list-group list-group-flush">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
+				{todos.map((todo, index)=>(
+					<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+					{todo}
+					<button onClick={()=>delTask(index)} className="btn btn-outline-danger btn-sm rounded-pill">X</button>
+				</li>
+				))}
 			</ul>
 			<div className="card-footer">
                Card footer
             </div>
 
         </div>
-	  </div>
+	  
 	);
 };
 
